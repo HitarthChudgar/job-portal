a1d9c6a73b6ffc5c9fc0d170ce134c33821b7bfb
Hitarth Chudgar <hitarthchudgar@gmail.com>
2024-10-14 16:28:11 -0400

    feat(joblisting): added json server and spinner component

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,19 +1,46 @@
-import jobs from "../jobs.json";
+import { useState } from "react";
+import { useEffect } from "react";
 import JobListing from "./JobListing";
+import Spinner from "./Spinner";
 
 const JobListings = ({ isHome = false }) => {
-  const jobListings = isHome ? jobs.slice(0, 3) : jobs; //showing only 3 jobs
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const apiUrl = isHome
+        ? "http://localhost:8000/jobs?_limit=3"
+        : "http://localhost:8000/jobs";
+      try {
+        const res = await fetch(apiUrl);
+        if (!res.ok) throw new Error(`Fetch failed with status: ${res.status}`);
+        const data = await res.json();
+        console.log(data);
+        setJobs(data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchJobs();
+  }, []);
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Browse Jobs
+          {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobListings.map((job) => (
-            <JobListing key={job.id} job={job} /> //create a child component
-          ))}
-        </div>
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job) => (
+              <JobListing key={job.id} job={job} /> //create a child component
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
