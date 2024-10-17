59ff3a4541093e84125166068057a50c2acf4ac7
Hitarth Chudgar <hitarthchudgar@gmail.com>
2024-10-16 22:46:46 -0400

    fix(joblisting): :pencil2: fixed the url format and got rid of http to accomdoate proxy

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -9,7 +9,7 @@ const JobListings = ({ isHome = false }) => {
 
   useEffect(() => {
     const fetchJobs = async () => {
-      const apiUrl = isHome ? "http://api/jobs?_limit=3" : "http://api/jobs";
+      const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
       try {
         const res = await fetch(apiUrl);
         if (!res.ok) throw new Error(`Fetch failed with status: ${res.status}`);
