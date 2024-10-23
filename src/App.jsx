c7ff471ab7f510a0490176a12f7d6ed88213c3e6
Hitarth Chudgar <hitarthchudgar@gmail.com>
2024-10-22 20:28:31 -0400

    feat: add job deletion functionality
    - Introduced `deleteJob` function in `App.jsx` to handle job deletion via API.
    - Updated `JobPage.jsx` to include a delete button with confirmation prompt, utilizing `useNavigate` for redirection post-deletion.

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import JobPage, { jobLoader } from "./pages/JobPage";
 import AddJob from "./pages/AddJob";
 
 const App = () => {
+  // add job
   const addJob = async (newJob) => {
     const res = await fetch("/api/jobs/", {
       method: "POST",
@@ -22,11 +23,25 @@ const App = () => {
     console.log(res);
     return res; // Optionally return the response if needed
   };
+
+  // delete job
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: "DELETE",
+    });
+    console.log(res);
+    return res;
+  };
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
-        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+        <Route
+          path="/jobs/:id"
+          element={<JobPage deleteJob={deleteJob} />}
+          loader={jobLoader}
+        />
         <Route path="/add-job" element={<AddJob addJobSubmit={addJob} />} />
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="*" element={<NotFoundPage />} />
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,8 +1,9 @@
 import { useParams, useLoaderData } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-const JobPage = () => {
+const JobPage = ({ deleteJob }) => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const job = useLoaderData();
   //   const [job, setJob] = useState(null);
@@ -24,6 +25,17 @@ const JobPage = () => {
   //     };
   //     fetchJob();
   //   }, []);
+
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm(
+      "Are you sure you want to delete this listing?"
+    );
+    if (!confirm) return;
+
+    deleteJob(jobId);
+
+    navigate("/jobs");
+  };
   return (
     <>
       <section>
@@ -102,7 +114,10 @@ const JobPage = () => {
                 >
                   Edit Job
                 </Link>
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                <button
+                  onClick={() => onDeleteClick(job.id)}
+                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                >
                   Delete Job
                 </button>
               </div>
